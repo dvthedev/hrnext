728668f434fc141e33d4ad94afc4c593d45e1292
Viraj Kaushalye <virajkaushalye1@gmail.com>
2024-04-11 00:00:12 +0530

    feat[ui]: improved sidenav link active status

--- src/components/SideNavLink.tsx
+++ src/components/SideNavLink.tsx
@@ -20,18 +20,25 @@ const SideNavLink: React.FC<SideNavLinkProps> = ({
   return (
     <Link
       href={path}
-      className={`flex items-center gap-2 p-2 hover:text-primary hover:font-medium transition-all hover:bg-muted rounded-full cursor-pointer ${
+      className={`flex h-full justify-between text-lg items-center py-1 pl-4 hover:text-primary hover:font-medium transition-all hover:bg-muted rounded-full cursor-pointer ${
         activePath === `/${path}` ? 'text-primary font-medium' : ''
       }`}
     >
-      <SideNavLinkIcon className="w-6 aspect-auto" />
-      <span>{title}</span>
+      <div className="flex gap-2 flex-1 py-1">
+        <SideNavLinkIcon className="w-6 aspect-auto" />
+        <span>{title}</span>
 
-      {badge && (
-        <Badge className="ml-auto flex h-6 shrink-0 items-center justify-center rounded-full">
-          {badge}
-        </Badge>
-      )}
+        {badge && (
+          <Badge className="ml-auto mr-2 flex h-6 shrink-0 items-center justify-center rounded-full">
+            {badge}
+          </Badge>
+        )}
+      </div>
+      <div
+        className={`h-full w-1.5 ${
+          activePath === `/${path}` && `bg-primary rounded-md`
+        }`}
+      ></div>
     </Link>
   )
 }
